18daa3acdbb638cf1adec2c071ba049bec597176
FezzedOne <RenoFezzedOne@yandex.com>
2024-06-09 12:22:23 -0400

    v1.3: Fixed xSIP item entry formatting.

--- fezzedtech-load.lua
+++ fezzedtech-load.lua
@@ -13,9 +13,33 @@ if sipFound then
     else
         goto skipSip
     end
+
+    local function getPath(itemName)
+        local sipItems = assets.json("/sipItemDump.json")
+        for _, item in pairs(sipItems) do
+            if item.name == itemName then
+                return item.path, item.fileName
+            end
+        end
+        return "/", "fezzedtech-items.json"
+    end
+
     local fezzedTechItems = assets.json("/fezzedtech-items.json")
     for _, item in pairs(fezzedTechItems) do
-        table.insert(sipCustomItems, item)
+        local parameters = item.parameters or jobject{}
+        local path, fileName = getPath(item.name)
+        table.insert(sipCustomItems, {
+            path = path,
+            fileName = fileName,
+            name = item.name,
+            parameters = parameters,
+            rarity = "legendary",
+            directives = parameters.directives,
+            shortdescription = parameters.shortdescription or (item.name .. "^orange;<custom>^reset;"),
+            icon = type(parameters.inventoryIcon) == "string" and parameters.inventoryIcon or "/assetmissing.png",
+            race = "generic",
+            category = parameters.category or "tool",
+        })
     end
     assets.erase("/sipCustomItems.json")
     assets.add("/sipCustomItems.json", sipCustomItems)
