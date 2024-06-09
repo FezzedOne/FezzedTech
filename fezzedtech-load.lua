2cfa650053afaa09bc551110e730211f6e58acba
FezzedOne <RenoFezzedOne@yandex.com>
2024-06-09 11:15:19 -0400

    v1.3: Add some FezzedTech items to SIP. Requires xSB-2 or OpenStarbound.

---
+++ fezzedtech-load.lua
@@ -0,0 +1,22 @@
+-- FezzedTech loading script --
+
+if xsb then
+    sb.logInfo("[FezzedTech] Detected xSB-2 v" .. xsb.version() .. ".")
+else
+    sb.logInfo("[FezzedTech] Detected OpenStarbound or similar.")
+end
+
+local sipFound, sipCustomItems = pcall(assets.json, "/sipCustomItems.json")
+if sipFound then
+    if pcall(assets.bytes, "/xSIP.lua") then
+        sb.logInfo("[FezzedTech] Detected xSIP; adding FezzedTech items...")
+    else
+        sb.logInfo("[FezzedTech] Detected Spawnable Item Pack; adding FezzedTech items...")
+    end
+    local fezzedTechItems = assets.json("/fezzedtech-items.json")
+    for _, item in pairs(fezzedTechItems) do
+        table.insert(sipCustomItems, item)
+    end
+    assets.erase("/sipCustomItems.json")
+    assets.add("/sipCustomItems.json", sipCustomItems)
+end
