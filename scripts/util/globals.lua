6bd4e415b210f26494e102bb89365a8c12f7079c
FezzedOne <RenoFezzedOne@yandex.com>
2024-06-18 20:11:17 -0400

    Whoops. NPC wielding should be properly fixed now.

--- scripts/util/globals.lua
+++ scripts/util/globals.lua
@@ -5,6 +5,10 @@ if not (entity or player or projectile) then
 end
 local uuid = player and player.uniqueId()
     or (projectile.sourceEntity and world.entityUniqueId(projectile.sourceEntity()) or entity.uniqueId())
+if not uuid then
+    globals = {}
+    goto skipGlobals
+end
 math.__fezTech = type(math.__fezTech) == "table" and math.__fezTech or {}
 math.__fezTech[uuid] = type(math.__fezTech[uuid]) == "table" and math.__fezTech[uuid] or {}
 globals = math.__fezTech[uuid]
