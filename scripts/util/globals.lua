2387ca1867b575c24ee4a90d4d2cb51da4c5dc40
FezzedOne <RenoFezzedOne@yandex.com>
2024-06-18 20:07:38 -0400

    Fixed potential issues with NPCs wielding weapons.

--- scripts/util/globals.lua
+++ scripts/util/globals.lua
@@ -1,10 +1,11 @@
 -- Script to initialise globals. Intended to be invoked via `require` in `init`.
 if not (entity or player or projectile) then
-    -- Throw an error if required callbacks are missing. This is an issue on stock Starbound.
-    error("[FezzedTech] Attempted to initialise globals without required uniqueId!")
+    globals = {}
+    goto skipGlobals
 end
 local uuid = player and player.uniqueId()
     or (projectile.sourceEntity and world.entityUniqueId(projectile.sourceEntity()) or entity.uniqueId())
 math.__fezTech = type(math.__fezTech) == "table" and math.__fezTech or {}
 math.__fezTech[uuid] = type(math.__fezTech[uuid]) == "table" and math.__fezTech[uuid] or {}
 globals = math.__fezTech[uuid]
+::skipGlobals::
